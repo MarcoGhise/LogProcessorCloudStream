41ec6584ffa7c8fe626c86f7ea4b3013e56a40ff
MarcoGhise <MarcoGhise@users.noreply.github.com>
2017-06-20 14:12:05 +0200

    Create FileSourceApplication.java

---
+++ filelogkafka-source/src/main/java/it/springboot/filelogkafka/FileSourceApplication.java
@@ -0,0 +1,13 @@
+package it.springboot.filelogkafka;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileSourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileSourceApplication.class, args);
+	}
+}
