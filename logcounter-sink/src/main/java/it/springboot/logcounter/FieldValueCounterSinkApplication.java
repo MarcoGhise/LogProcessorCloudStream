b0cc12affeb864d00aa7053e8ea9bba2bc75c09d
MarcoGhise <MarcoGhise@users.noreply.github.com>
2017-06-20 14:25:15 +0200

    Create FieldValueCounterSinkApplication.java

---
+++ logcounter-sink/src/main/java/it/springboot/logcounter/FieldValueCounterSinkApplication.java
@@ -0,0 +1,16 @@
+package it.springboot.logcounter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Ilayaperumal Gopinathan
+ */
+@SpringBootApplication
+public class FieldValueCounterSinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FieldValueCounterSinkApplication.class, args);
+	}
+
+}
